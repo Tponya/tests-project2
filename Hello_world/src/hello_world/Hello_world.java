6fe72d5adcab06f250def0caea5daa78358a51e6
Tony Qi <t.qi@surreyschools.ca>
2022-11-14 10:44:15 -0800

    Hello_world/

---
+++ Hello_world/src/hello_world/Hello_world.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello_world;
+
+/**
+ *
+ * @author t.qi
+ */
+public class Hello_world {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    /**
+     * Yo yo yo yo yoyoyoyoyoyo
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
